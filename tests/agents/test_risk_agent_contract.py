c5c51ab8733584f0d37c76fc1ad36440fe3cbd79
nidish_m <nidish124@gmail.com>
2025-12-28 17:03:28 +0530

    add CI pipeline

--- tests/agents/test_risk_agent_contract.py
+++ tests/agents/test_risk_agent_contract.py
@@ -2,6 +2,11 @@ from contracts.agents.risk_agent_contract import RiskAgentInput
 from contracts.credit_application import CreditApplication
 from agents.risk_agent import RiskScoringAgent
 from agents.ollama_provider import OllamaProvider
+import os
+from dotenv import load_dotenv
+from api.factories import FakeLLM
+
+load_dotenv()
 
 def test_risk_agent_input_contract():
     app = CreditApplication(
@@ -18,8 +23,14 @@ def test_risk_agent_input_contract():
         declared_assets_value=800000
     )
 
+    execution = os.getenv("EXECUTION_MODE")
+
+    if execution == "ci":
+        curr_llm = FakeLLM()
+    curr_llm = OllamaProvider()
+
     agent = RiskScoringAgent(
-        llm=OllamaProvider(),
+        llm=curr_llm,
         prompt_template=open("agents/prompts/risk_prompt.txt").read()
     )
 
